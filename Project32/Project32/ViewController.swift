e8e5925f487699f4d0c5cb7a8d51311007a58728
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-11-18 21:16:06 -0600

    Setup project

---
+++ Project32/Project32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project32
+//
+//  Created by Álvaro Ávalos Hernández on 18/11/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
